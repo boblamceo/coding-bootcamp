9cf87c26d3f1ce069c6d939dd898a07fe0b90efd
Bob Lam <boblam.ceo@gmail.com>
2019-09-15 18:22:15 +0800

    continue typescript course

--- packages/video-lectures/typescript-course/web/src/models/User.ts
+++ packages/video-lectures/typescript-course/web/src/models/User.ts
@@ -1,8 +1,8 @@
-import { Eventing } from './Eventing';
-import { Sync } from './Sync';
+import { Model } from './Model';
 import { Attributes } from './Attributes';
-
-import { AxiosResponse } from 'axios';
+import { ApiSync } from './ApiSync';
+import { Eventing } from './Eventing';
+import { userInfo } from 'os';
 
 export interface UserProps {
     id?: number;
@@ -12,52 +12,10 @@ export interface UserProps {
 
 const rootUrl = 'http://localhost:3000/users';
 
-export class User {
-    private events: Eventing = new Eventing();
-    public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-    public attributes: Attributes<UserProps>;
-
-    constructor(attrs: UserProps) {
-        this.attributes = new Attributes<UserProps>(attrs);
-    }
-
-    get on() {
-        return this.events.on;
-    }
-
-    get trigger() {
-        return this.events.trigger;
-    }
-
-    get get() {
-        return this.attributes.get;
-    }
-
-    set(update: UserProps): void {
-        this.attributes.set(update);
-        this.events.trigger('change');
-    }
-
-    fetch(): void {
-        const id = this.attributes.get('id');
-
-        if (typeof id !== 'number') {
-            throw new Error('Cannot fetch without an id');
-        }
-
-        this.sync.fetch(id).then((res: AxiosResponse): void => {
-            this.set(res.data);
-        });
-    }
-
-    save(): void {
-        this.sync
-            .save(this.attributes.getAll())
-            .then((response: AxiosResponse): void => {
-                this.trigger('save');
-            })
-            .catch(() => {
-                this.trigger('error');
-            });
+export class User extends Model<UserProps> {
+    static buildUser(attrs: UserProps) {
+        return new User(new Attributes<UserProps>(attrs), new Eventing(), new ApiSync<UserProps>(rootUrl));
     }
 }
+
+const user = User.buildUser({});
